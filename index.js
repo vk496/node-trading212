4cfec2e92cd183549ae85f9ccc7a9f7cf0c3dd4c
Valentin KIVACHUK BURDA <vk18496@gmail.com>
2023-01-01 18:28:15 +0100

    Restrcuture code to use events and automatically refresk token

--- index.js
+++ index.js
@@ -18,102 +18,127 @@ var initInfoUrl = '/rest/v3/init-info';
 var instrumentsUrl = '/rest/v1/instruments/';
 */
 
-var transport = null;
 
 var ws = null;
 
 
 class Trading212 extends EventEmitter {
-    constructor(ENV, CUSTOMER_SESSION) {
+    constructor(ENV, LOGIN_TOKEN, dUUID) {
         super();
         const wssUrl = `wss://${ENV.toLowerCase()}.trading212.com/streaming/events/?app=WC4&appVersion=2.2.85&EIO=3&transport=websocket`;
 
         baseUrl = "https://" + ENV.toLowerCase() + baseUrl;
 
-        var headers = {
+        this.headers = {
             "Accept": "application/json",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
             "Sec-Fetch-Dest": "empty",
             "Sec-Fetch-Mode": "cors",
             "Sec-Fetch-Site": "same-origin",
+            "X-Trader-Client": `application=WC4, version=1.0.0, dUUID=${dUUID}`,
             "Sec-GPC": 1,
             "Content-Type": "application/json",
-            "Cookie": `CUSTOMER_SESSION=${CUSTOMER_SESSION}; TRADING212_SESSION_${ENV.toUpperCase()}=${CUSTOMER_SESSION}`
+            "Cookie":
+                `LOGIN_TOKEN=${LOGIN_TOKEN}; ` +
+                ""
         }
 
-        transport = axios.create({
+        this.transport = axios.create({
             withCredentials: true,
-            headers: headers
+            headers: this.headers
         });
 
-        var opts = { headers: headers };
-        ws = new websocketClient(wssUrl, opts);
+        this.transport
+            .get(baseUrl + "/rest/v1/webclient/authenticate").then(res => {
 
-        ws.on('open', () => {
-            this.emit('connection-established')
-        });
+                this.headers["Cookie"] = res.headers["set-cookie"].map(f => { return f.match(/(.+?; )/)[0] }).join("")
+                this.transport = axios.create({
+                    withCredentials: true,
+                    headers: this.headers
+                });
 
-        ws.on('close', () => {
-            console.log("ws closed");
-            this.emit('trading212-ws-closed');
-        })
+                this.emit('refresh-token', res.data);
+            })
+            .catch(err => {
+                throw new Error("Session cookie expired! : " + err)
+            })
 
-        ws.on('sid', (data) => {
-            //sid received
-        });
+        var opts = { headers: this.headers };
 
-        ws.on('message', (data) => {
-            if (data == 3) {
-                ws.send(2);
-            }
+        this.on("refresh-token", () => {
+            ws = new websocketClient(wssUrl, opts);
 
-            if (data.substring(0, '42["platform-message-sync"'.length) == '42["platform-message-sync"') {
-                this.subscribeRoute('WEBPLATFORM');
-                this.subscribeRoute('ACCOUNT');
-                ws.send('42["acc"]');
-                this.emit('platform-subscribed');
-            } else if (data.substring(0, '42["q"'.length) == '42["q"') {
-                data = data.substring('42["q"'.length);
-                data = data.substring(2, data.length - 1);
-                data = data.split("|");
-                data = {
-                    ticker: data[0],
-                    bid: data[1],
-                    ask: data[2],
+            ws.on('open', () => {
+                this.emit('connection-established')
+            });
+
+            ws.on('close', () => {
+                console.log("ws closed");
+                this.emit('trading212-ws-closed');
+            })
+
+            ws.on('sid', (data) => {
+                //sid received
+            });
+
+            ws.on('message', (data) => {
+                if (data == 3) {
+                    ws.send(2);
                 }
-                this.emit('price', data);
-            } else if (data.substring(0, '42["q-sync"'.length) == '42["q-sync"') {
-                data = data.substring('42["q-sync"'.length);
-                data = data.substring(2, data.length - 1);
-                data = data.split("|");
-                data = {
-                    ticker: data[0],
-                    bid: data[1],
-                    ask: data[2],
-                }
-                this.emit('price', data);
-            } else if (data.substring(0, '42["working-schedule-sync"'.length) == '42["working-schedule-sync"') {
 
-            } else if (data.substring(0, 9) == '42["acc",') {
-                data = data.substring(9);
-                data = data.substring(0, data.length - 1);
-                data = JSON.parse(data);
-                this.emit('account', data);
-            } else if (data.substring(0, 12) == '42["acc-re",') {
-                data = data.substring(12);
-                data = data.substring(0, data.length - 1);
-                data = JSON.parse(data);
-                this.emit('reaccount', data);
-            } else if (data.substring(0, '42["buckets-sync",'.length) == '42["buckets-sync",') {
-                data = data.substring('42'.length);
-                data = JSON.parse(data);
-                this.emit('buckets', data[1]);
-            } else {
-                console.log(data);
-            }
+                if (data.substring(0, '42["platform-message-sync"'.length) == '42["platform-message-sync"') {
+                    this.subscribeRoute('WEBPLATFORM');
+                    this.subscribeRoute('ACCOUNT');
+                    ws.send('42["acc"]');
+                    this.emit('platform-subscribed');
+                } else if (data.substring(0, '42["q"'.length) == '42["q"') {
+                    data = data.substring('42["q"'.length);
+                    data = data.substring(2, data.length - 1);
+                    data = data.split("|");
+                    data = {
+                        ticker: data[0],
+                        bid: data[1],
+                        ask: data[2],
+                    }
+                    this.emit('price', data);
+                } else if (data.substring(0, '42["q-sync"'.length) == '42["q-sync"') {
+                    data = data.substring('42["q-sync"'.length);
+                    data = data.substring(2, data.length - 1);
+                    data = data.split("|");
+                    data = {
+                        ticker: data[0],
+                        bid: data[1],
+                        ask: data[2],
+                    }
+                    this.emit('price', data);
+                } else if (data.substring(0, '42["working-schedule-sync"'.length) == '42["working-schedule-sync"') {
+
+                } else if (data.substring(0, 9) == '42["acc",') {
+                    data = data.substring(9);
+                    data = data.substring(0, data.length - 1);
+                    data = JSON.parse(data);
+                    this.emit('account', data);
+                } else if (data.substring(0, 12) == '42["acc-re",') {
+                    data = data.substring(12);
+                    data = data.substring(0, data.length - 1);
+                    data = JSON.parse(data);
+                    this.emit('reaccount', data);
+                } else if (data.substring(0, '42["buckets-sync",'.length) == '42["buckets-sync",') {
+                    data = data.substring('42'.length);
+                    data = JSON.parse(data);
+                    this.emit('buckets', data[1]);
+                } else {
+                    // console.log(data);
+                }
+            });
         });
 
+
+    }
+
+    close() {
+        ws.close()
     }
 
     subscribeRoute(route) {
@@ -135,7 +160,7 @@ class Trading212 extends EventEmitter {
     }
 
     getCurrentPrice(instruments) {
-        transport
+        this.transport
             .post(baseUrl + priceUrl, instruments).then(res => {
                 res.data.ticker = res.data.instrumentCode;
                 this.emit('price', res.data);
@@ -148,17 +173,18 @@ class Trading212 extends EventEmitter {
         this.subscribeRoute('BUCKETS');
     }
 
-    async getBucketId(bucketName) {
+    getBucketId(bucketName, bucketID) {
         this.subscribeRoute('BUCKETS');
-        const [response] = await once(this, 'buckets');
-
-        return response
-            .find(arr => arr.settings.name == bucketName)
-            .id;
+        this.on('buckets', (response) => {
+            const bucketId = response
+                .find(arr => arr.settings.name == bucketName)
+                .id;
+                bucketID(bucketId)
+        })
     }
 
     fundBucket(bucketID, amount, distribution_type) {
-        transport
+        this.transport
             .get(baseUrl + `/rest/autoinvest/buckets/${bucketID}/fund?amount=${amount.toFixed(2)}&distributionType=${distribution_type}`)
             .then(resGET => {
                 // Confirmed we can make the operation
@@ -169,26 +195,24 @@ class Trading212 extends EventEmitter {
                     "distributionType": distribution_type,
                     "orders": resGET.data.buys,
                 }
-                transport
+                this.transport
                     .post(baseUrl + `/rest/autoinvest/buckets/${bucketID}/fund?distributionType=${distribution_type}`, postData)
                     .then(res => {
                         this.emit('bucket-funded', res.data);
                     })
-                    .catch(err => { console.log(err) })
 
             })
-            .catch(err => { console.log(err) })
     }
 
     getAvailableEquities() {
-        transport
+        this.transport
             .get(baseUrl + availableEquitiesUrl)
             .then(res => { this.emit('equity-data', res.data.items); })
             .catch(err => { console.log(err) })
     }
 
     getHotlist(period, delta) {
-        transport
+        this.transport
             .get(hotListUrl + period + "/" + delta)
             .then(res => { this.emit('hotlist', period, delta, res.data); })
             .catch(err => { console.log(err) })
@@ -206,7 +230,7 @@ class Trading212 extends EventEmitter {
 
     validateOrder(instrumentCode, orderType, stopPrice, limitPrice, quantity, timeValidity) {
         return new Promise(function (resolve, reject) {
-            transport
+            this.transport
                 .post(baseUrl + orderUrl + '/validate', {
                     instrumentCode: instrumentCode,
                     orderType: orderType,
@@ -228,7 +252,7 @@ class Trading212 extends EventEmitter {
     }
 
     placeOrder(instrumentCode, orderType, stopPrice, limitPrice, quantity, timeValidity) {
-        transport
+        this.transport
             .post(baseUrl + orderUrl, {
                 instrumentCode: instrumentCode,
                 orderType: orderType,
@@ -250,7 +274,7 @@ class Trading212 extends EventEmitter {
 
     deleteOrder(orderId) {
         console.log('trying to delete order');
-        transport
+        this.transport
             .delete(baseUrl + orderUrl + '/' + orderId).then(res => {
                 if (res.hasOwnProperty("data") && res.data.hasOwnProperty("account")) {
                     this.emit('account', res.data);
